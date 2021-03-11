d8cd724316b9b3490afa0e6d83b09cd5b77acfaa
dimrio323 <62644864+dimrio323@users.noreply.github.com>
2021-03-11 08:57:17 +0500

    Выход у студента

--- src/containers/TestSelection/testSelection.js
+++ src/containers/TestSelection/testSelection.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import './testSelection.css'
 import Loader from '../../components/Loader/Loader'
 import { nanoid } from 'nanoid'
 import { connect } from 'react-redux'
+import axios from '../../axios/axios'
 
 class testSelection extends Component {
 
@@ -31,6 +32,23 @@ class testSelection extends Component {
     }
 
     render() {
+        const signout = () => {
+            axios.get('/student/choice?type=signout&studkey=' + this.props.sessionStudent)
+            .then(response => {
+                console.log(response.data);
+                if (response.data.type == 'success') {
+                    localStorage.removeItem('sessionStudent')
+                    localStorage.removeItem('fullName')
+                    localStorage.removeItem('group')
+                    localStorage.removeItem('uid')
+                    localStorage.removeItem('expirationDateStudent')
+                    window.location.href = '/'
+                }
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+
         return (
             <React.Fragment>
                 <div className="testSelection">
@@ -38,6 +56,7 @@ class testSelection extends Component {
                         name={this.props.name}
                         group={this.props.group}
                     />
+                    <button onClick={signout} className="signout" >Новая авторизация</button>
                     <div className="testSelectionContent">
                         {
                             this.state.loading
@@ -67,7 +86,8 @@ class testSelection extends Component {
 function mapStateToProps(state) {
     return {
         name: state.student.name,
-        group: state.student.group
+        group: state.student.group,
+        sessionStudent: state.student.sessionStudent
     }
 }
 
